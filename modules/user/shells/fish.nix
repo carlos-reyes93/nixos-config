20593eb6ff29c5e59a759d9f2f32c97c337d87f4
Carlos Reyes <contacto@carlosreyc.dev>
2025-12-23 09:42:56 -0600

    feat: properly set-up nvim as git editor
    Signed-off-by: Carlos Reyes <contacto@carlosreyc.dev>

--- modules/user/shells/fish.nix
+++ modules/user/shells/fish.nix
@@ -11,6 +11,12 @@
   config = lib.mkIf config.fish.enable {
     environment.shells = with pkgs; [fish];
     users.defaultUserShell = pkgs.fish;
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+      shellInit = ''
+
+        set -gx EDITOR nvim
+      '';
+    };
   };
 }
